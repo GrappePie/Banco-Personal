6108be4a710eff47edf321253f5bc91824ad70f4
GrappePie <grappepie@gmail.com>
2026-04-25 02:53:03 -0600

    Add country to currency helpers

---
+++ src/currency/currency.ts
@@ -0,0 +1,60 @@
+export const COUNTRY_TO_CURRENCY: Record<string, string> = {
+  AR: 'ARS',
+  AU: 'AUD',
+  BR: 'BRL',
+  CA: 'CAD',
+  CH: 'CHF',
+  CL: 'CLP',
+  CN: 'CNY',
+  CO: 'COP',
+  CZ: 'CZK',
+  DK: 'DKK',
+  ES: 'EUR',
+  FR: 'EUR',
+  DE: 'EUR',
+  GB: 'GBP',
+  HK: 'HKD',
+  HU: 'HUF',
+  ID: 'IDR',
+  IL: 'ILS',
+  IN: 'INR',
+  IS: 'ISK',
+  JP: 'JPY',
+  KR: 'KRW',
+  MD: 'MDL',
+  MX: 'MXN',
+  MY: 'MYR',
+  NO: 'NOK',
+  NZ: 'NZD',
+  PE: 'PEN',
+  PH: 'PHP',
+  PL: 'PLN',
+  RO: 'RON',
+  RU: 'RUB',
+  SE: 'SEK',
+  SG: 'SGD',
+  TH: 'THB',
+  TR: 'TRY',
+  US: 'USD',
+  ZA: 'ZAR'
+}
+
+export function getCurrencyForCountry(countryCode?: string) {
+  if (!countryCode) {
+    return 'MXN'
+  }
+
+  return COUNTRY_TO_CURRENCY[countryCode.toUpperCase()] ?? 'USD'
+}
+
+export function formatMoney(amount: number, currency: string, locale = 'es-MX') {
+  try {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency,
+      maximumFractionDigits: 2,
+    }).format(amount)
+  } catch {
+    return `${amount.toFixed(2)} ${currency}`
+  }
+}
