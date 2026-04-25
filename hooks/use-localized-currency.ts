1f1cc5acd60755823cb66f12e77c52e3c9ec56fb
GrappePie <grappepie@gmail.com>
2026-04-25 03:13:00 -0600

    Support converting localized input back to MXN

--- hooks/use-localized-currency.ts
+++ hooks/use-localized-currency.ts
@@ -6,17 +6,33 @@ export function useLocalizedCurrency() {
   const { countryCode, language } = useI18n()
   const currencyConversion = useCurrencyConversion(countryCode)
 
+  const activeCurrency = currencyConversion.rate
+    ? currencyConversion.targetCurrency
+    : currencyConversion.baseCurrency
+
   const formatCurrency = (amountInMxn: number) => {
     const convertedAmount = currencyConversion.convertFromMxn(amountInMxn) ?? amountInMxn
-    const currency = currencyConversion.rate
-      ? currencyConversion.targetCurrency
-      : currencyConversion.baseCurrency
 
-    return formatMoney(convertedAmount, currency, language)
+    return formatMoney(convertedAmount, activeCurrency, language)
+  }
+
+  const convertFromMxn = (amountInMxn: number) => {
+    return currencyConversion.convertFromMxn(amountInMxn) ?? amountInMxn
+  }
+
+  const convertToMxn = (amountInActiveCurrency: number) => {
+    if (!currencyConversion.rate) {
+      return amountInActiveCurrency
+    }
+
+    return amountInActiveCurrency / currencyConversion.rate
   }
 
   return {
     ...currencyConversion,
+    activeCurrency,
     formatCurrency,
+    convertFromMxn,
+    convertToMxn,
   }
 }
